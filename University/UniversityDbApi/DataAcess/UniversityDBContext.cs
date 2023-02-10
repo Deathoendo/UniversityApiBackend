54245a7496330cb6627404e45cad728247ef5f1c
Deathoendo <83551997+Deathoendo@users.noreply.github.com>
2023-02-10 14:55:10 -0400

    Apibackend
    UniversityApiBackend

---
+++ University/UniversityDbApi/DataAcess/UniversityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityDbApi.Models.DataModels;
+
+namespace UniversityDbApi.DataAcess
+{
+    public class UniversityDBContext : DbContext
+    {
+       
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options) { 
+
+        }
+        //TODO: add DbSet (Tables of Data Base)
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ University/UniversityDbApi/Program.cs
@@ -0,0 +1,39 @@
+//1. Using to work with entity framerwork
+using Microsoft.EntityFrameworkCore;
+using UniversityDbApi.DataAcess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//2. Connection with sql Server Express
+
+const string CONNECTIONNAME = "UniversityDb";
+var connectionString =builder.Configuration.GetConnectionString(CONNECTIONNAME);
+
+//3. Add context to services of builder
+builder.Services.AddDbContext<UniversityDBContext>(options => options.UseSqlServer(connectionString));
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
